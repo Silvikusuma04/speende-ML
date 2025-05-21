3052f4e7548920307268c12ae2fde504933c76ff
silvikusuma04 <silvikusuma043@gmail.com>
2025-05-21 23:29:11 +0700

    update port

--- Fitur_1/main.py
+++ Fitur_1/main.py
@@ -168,5 +168,6 @@ def predict():
 
     return render_template_string(html_template, result=result, pos_reason=pos_reason, neg_reason=neg_reason)
 
-if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8080)
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8080))
+    app.run(debug=True, host='0.0.0.0',port=port)
